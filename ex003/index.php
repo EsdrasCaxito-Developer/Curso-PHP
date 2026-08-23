ae33ddd7b809a96906284b0647426eb68f44a8f6
Esdras Domingos <esdrascaxitodeveloper@yahoo.com>
2026-08-23 19:16:03 +0100

    Tipos primitivos do PHP

---
+++ ex003/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tipos primitivos em PHP</title>
+</head>
+
+<body>
+    <h1>Teste de tipos primitivos</h1>
+
+    <?php
+    //0x - Hexadecimal
+    //0b - Binário
+    //0 -  Octal
+
+    // $n = "Esdras";
+    // var_dump($n);
+
+    // $n = (int) 3e2; // 3 * 10^2
+    // echo "O valor é $n";
+    // var_dump($n);
+
+    // $n = (float) "950";
+    // var_dump($n);
+
+    $casado = true;
+    //var_dump($casado);
+    print("O valor para casado é $casado");
+    ?>
+</body>
+
+</html>
